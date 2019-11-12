ffe09af9d51e4a2980662ce5da591b0cbfaf7172
Rahul Raveendran V P <rahul.pmna@gmail.com>
2019-11-12 12:21:16 +0530

    using new aar

--- app/src/main/java/com/blueshift/reads/framework/ReadsApplication.java
+++ app/src/main/java/com/blueshift/reads/framework/ReadsApplication.java
@@ -1,13 +1,13 @@
 package com.blueshift.reads.framework;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import androidx.core.content.ContextCompat;
+import androidx.multidex.MultiDex;
 
 import com.blueshift.Blueshift;
-import com.blueshift.inappmessage.InAppApiCallback;
 import com.blueshift.model.Configuration;
 import com.blueshift.reads.BuildConfig;
 import com.blueshift.reads.R;
@@ -27,6 +27,13 @@ public class ReadsApplication extends Application {
     private final String PREF_KEY = "pref_key";
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
 
@@ -44,9 +51,12 @@ public class ReadsApplication extends Application {
         configuration.setDefaultNotificationChannelName("Random");
         configuration.setEnableAutoAppOpenFiring(true);
 
+        configuration.setPushEnabled(true);
+
         configuration.setInAppEnabled(true);
         configuration.setJavaScriptForInAppWebViewEnabled(true);
-        configuration.setInAppManualTriggerEnabled(true);
+//        configuration.setInAppManualTriggerEnabled(true);
+        configuration.setInAppBackgroundFetchEnabled(true);
 
         // configuration.setOfferDisplayPage(OfferDisplayActivity.class);
         // configuration.setDialogTheme(R.style.dialog_theme);
@@ -55,6 +65,28 @@ public class ReadsApplication extends Application {
         configuration.setApiKey(BuildConfig.API_KEY);
 
         Blueshift.getInstance(this).initialize(configuration);
+
+        // overriding the in-ap api call
+//        Blueshift
+//                .getInstance(this)
+//                .fetchInAppMessages(new InAppApiCallback() {
+//                    @Override
+//                    public void onApiCallComplete() {
+//
+//                    }
+//                });
+
+        // overriding the clicks on in-app
+//        Blueshift
+//                .getInstance(this)
+//                .setInAppActionCallback(new InAppActionCallback() {
+//                    @Override
+//                    public void onAction(String name, JSONObject args) {
+//                        Log.d("Application", "InApp Action. name: " + name + ", args: " + args);
+//                        // name - name of the action
+//                        // args - the arguments for the action
+//                    }
+//                });
 
         // app install check
         SharedPreferences sp = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
