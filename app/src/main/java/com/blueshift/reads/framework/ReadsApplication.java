c010a1d10d01f1f5811c107ae2a6699f50f929d8
Rahul Raveendran V P <rahul.pmna@gmail.com>
2020-06-05 10:33:13 +0530

    documented the blueshift config class usage

--- app/src/main/java/com/blueshift/reads/framework/ReadsApplication.java
+++ app/src/main/java/com/blueshift/reads/framework/ReadsApplication.java
@@ -24,54 +24,33 @@ import com.blueshift.reads.activity.ProductDetailsActivity;
 
 public class ReadsApplication extends Application {
 
-    private final String PREF_FILE = "pref_file";
-    private final String PREF_KEY = "pref_key";
-
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
-
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        /*
+        Helps to decide the level of logs from Blueshift Android SDK
+         */
         BlueshiftLogger.setLogLevel(BlueshiftLogger.VERBOSE);
 
-        Configuration configuration = new Configuration();
-        configuration.setAppIcon(R.mipmap.ic_launcher);
-
-        configuration.setProductPage(ProductDetailsActivity.class);
-        configuration.setCartPage(PlaceOrderActivity.class);
-
-        configuration.setLargeIconResId(R.mipmap.ic_launcher);
-        configuration.setSmallIconResId(R.drawable.ic_notification);
-
-        configuration.setNotificationColor(ContextCompat.getColor(this, R.color.colorPrimary));
-
-        configuration.setDefaultNotificationChannelName("Random");
-        configuration.setEnableAutoAppOpenFiring(true);
-
-        configuration.setPushEnabled(true);
-
-        configuration.setInAppEnabled(true);
-        configuration.setJavaScriptForInAppWebViewEnabled(true);
-//        configuration.setInAppManualTriggerEnabled(true);
-        configuration.setInAppBackgroundFetchEnabled(true);
-
-        // configuration.setOfferDisplayPage(OfferDisplayActivity.class);
-        // configuration.setDialogTheme(R.style.dialog_theme);
-        configuration.setBatchInterval(16 * 60 * 1000); // setting batch time as 16min
-
-        configuration.setApiKey(BuildConfig.API_KEY);
-
-        // use firebase instance id
-        configuration.setDeviceIdSource(Blueshift.DeviceIdSource.INSTANCE_ID);
-
+        /*
+        Following is the initialization part
+         */
+        Configuration configuration = getBlueshiftConfiguration();
+        configuration.setApiKey(BuildConfig.API_KEY); // Blueshift Event API Key
         Blueshift.getInstance(this).initialize(configuration);
 
+        trackNewInstalls();
+
+        /*
+        Below are samples for overriding SDK controls.
+         */
 //        override clicks on in-app messages
 //        Blueshift.getInstance(this).setInAppActionCallback(
 //                new InAppActionCallback() {
@@ -105,10 +84,65 @@ public class ReadsApplication extends Application {
 //                        // args - the arguments for the action
 //                    }
 //                });
+    }
 
+    private Configuration getBlueshiftConfiguration() {
+        Configuration configuration = new Configuration();
+
+        // This icon will be used in Notification as icons & placeholder image,
+        // if notification icons are not provided explicitly
+        configuration.setAppIcon(R.mipmap.ic_launcher);
+
+        // These methods are used for setting traditional deep-links with category
+        // heads-up: these methods will be deprecated soon
+        configuration.setProductPage(ProductDetailsActivity.class);
+        configuration.setCartPage(PlaceOrderActivity.class);
+        // configuration.setOfferDisplayPage(OfferDisplayActivity.class);
+        // configuration.setDialogTheme(R.style.dialog_theme);
+
+        // Following methods will let you set the large & small icons for Notification
+        configuration.setLargeIconResId(R.mipmap.ic_launcher);
+        configuration.setSmallIconResId(R.drawable.ic_notification);
+
+        // The following method will let you decide the color to be used in the Notification
+        configuration.setNotificationColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        // Following methods will help you setup Notification channel for Android O and above.
+        configuration.setDefaultNotificationChannelId("My-Notification-Channel-Id");
+        configuration.setDefaultNotificationChannelName("My-Notification-Channel-Name");
+        configuration.setDefaultNotificationChannelDescription("My-Notification-Channel-Description");
+
+        // This method tells the sdk to fire an app_open event automatically when
+        // user starts the application
+        configuration.setEnableAutoAppOpenFiring(true);
+
+        // This method allows you to disable push - default value is true
+        configuration.setPushEnabled(true);
+
+        // Following methods will help you set-up in-app messages
+        configuration.setInAppEnabled(true);
+        configuration.setJavaScriptForInAppWebViewEnabled(true);
+        configuration.setInAppBackgroundFetchEnabled(true);
+        // configuration.setInAppManualTriggerEnabled(true);
+
+        // This method let's you decide the interval of batch event api calls.
+        // Default value is 30min
+        configuration.setBatchInterval(16 * 60 * 1000); // setting batch time as 16min
+
+        // This method will let you decide what needs to be collected as device_id
+        // The default value is AdvertisingId. You can change it to Firebase Instance Id
+        // or a GUID using this method.
+        configuration.setDeviceIdSource(Blueshift.DeviceIdSource.INSTANCE_ID);
+
+        return configuration;
+    }
+
+    private void trackNewInstalls() {
         // app install check
+        String PREF_FILE = "pref_file";
         SharedPreferences sp = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
         if (sp != null) {
+            String PREF_KEY = "pref_key";
             boolean isNewInstall = sp.getBoolean(PREF_KEY, true);
             if (isNewInstall) {
                 // call event
